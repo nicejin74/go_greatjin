80ee5036962e4c0667d492741f85bd68a0567bcb
greatjin_x16_ubuntu <greatjin_x16_ubuntu@greatjin.com>
2024-08-28 17:48:27 +0900

    go_greatjin_test.go

---
+++ go_greatjin_test.go
@@ -0,0 +1,32 @@
+package go_greatjin
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "Test Add 1",
+			args: args{
+				a: 1,
+				b: 2,
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
